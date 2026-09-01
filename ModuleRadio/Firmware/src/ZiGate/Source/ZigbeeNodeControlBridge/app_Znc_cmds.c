ff01a700747a4e022c09719d17d3710899124649
Ivan Belokobylskiy <belokobylskij@gmail.com>
2026-09-01 12:22:16 +0400

    Bump firmware version to 0.3.24

--- ModuleRadio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/app_Znc_cmds.c
+++ ModuleRadio/Firmware/src/ZiGate/Source/ZigbeeNodeControlBridge/app_Znc_cmds.c
@@ -104,7 +104,7 @@
 #endif
 
 #ifndef VERSION
-#define VERSION    0x00030323
+#define VERSION    0x00030324
 #endif
 /****************************************************************************/
 /***    Type Definitions                          ***/
